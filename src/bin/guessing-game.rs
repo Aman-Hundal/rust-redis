e30ebf3ddb1457ec11920e170cf46e0c9a48963b
Aman Hundal <amanhundal19@gmail.com>
2025-04-06 14:01:52 -0700

    guessing game changes

--- src/bin/guessing-game.rs
+++ src/bin/guessing-game.rs
@@ -1,8 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Please enter a number");
     let mut guess = String::new();
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
 
     io::stdin()
     .read_line(&mut guess)
