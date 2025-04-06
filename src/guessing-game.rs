9134ca03ec744696e7873d0e36e3f8b4b55d075f
Aman Hundal <amanhundal19@gmail.com>
2025-04-06 13:35:15 -0700

    clean up of folders

--- src/guessing-game.rs
+++ src/guessing-game.rs
@@ -1,15 +1,14 @@
-user std::io;
+use std::io;
 
 fn main() {
     println!("Please enter a number");
     let mut guess = String::new();
 
     io::stdin()
-    .read_line(&mut guess);
+    .read_line(&mut guess)
+    .expect("Failed to read line");
 
     println!("You guessed: {}", guess)
-
-
 
 
 }
